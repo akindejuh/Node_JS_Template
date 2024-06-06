ceffd4a313c4169d328097c2cb13dfc14bbdc16e
Akindeju Oluwagbemiga Michael <akindejuhmikel@gmail.com>
2024-06-06 21:43:46 +0100

    Fix: Problem found in logger.ts

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,7 +1,8 @@
 import { createLogger, transports, format } from 'winston';
+import { EnvConfig } from './get-env';
 
-const environment = process.env.NODE_ENV;
-const logLevel = environment === 'development' ? 'debug' : 'warn';
+const environment = EnvConfig.nodeEnv;
+const logLevel = environment === 'development' ? 'debug' : 'info';
 
 const logger = createLogger({
   transports: [
@@ -15,7 +16,6 @@ const logger = createLogger({
   ],
 });
 
-// Handle Exceptions
 process.on('UnhandledRejection', error => {
   logger.error('Unhandled Rejection:', error);
 });
